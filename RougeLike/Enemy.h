b00f6cd0c90ba4783041bf128461d853525f343a
MacTheMink <aaronmcevoy1610@gmail.com>
2022-11-24 13:24:55 +0000

    enemy header

--- RougeLike/Enemy.h
+++ RougeLike/Enemy.h
@@ -1,24 +1,44 @@
 #pragma once
 
 #include <string>
+#include <random>
+#include <ctime>
+
+using std::string;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::time;
 
 class Enemy
 {
 private:
-	std::string m_name;
-	int m_maxHealth;
-	int m_currentHealth;
-	int m_xp;
-	int m_attackDamage;
+	string _name;
+	char _unitTile;
+
+	int _health;
+	int _attack;
+	int _xp;
+	int _defence;
+	int _level;
+
+	int _x;
+	int _y;
 
 public:
-	Enemy(std::string name, int maxHealth, int xp, int attackDamage);
+	Enemy(std::string name, char unitTile, int health, int attack, int defence, int xp, int level);
 	~Enemy();
 
 	//Getters and Setters
-	int getHealth();
-	int getXp();
-	int getAttackDamage();
+	void getPosition(int& x, int& y);
+	void setPosition(int x, int y);
+	int attackChance();
+	int getHealth();//Getting Enemy Health
+	int getXp();//Getting Enemy XP
+	int getAttackDamage(int attackChance);//Get damage done to enemy
+	string getEnemyName();
+	int getEnemyHealth();
+	char getMove(int playerX, int playerY);
+	char getChar();
 
 	void attack();
 };
